79b5b3d65c76c6a2087088c0a3cb76a3254a1a91
PritiChavanIBM <128017158+PritiChavanIBM@users.noreply.github.com>
2023-03-29 09:17:07 +0530

    Add files via upload

---
+++ Cucumber/Activities/FirstTest.java
@@ -0,0 +1,10 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class FirstTest extends BaseClass{
+    @Given("user is on the TS homepage")
+    public void openTSHomepage(){
+//WebDriverManager.firefoxdriver.setup();
+    }
+}
